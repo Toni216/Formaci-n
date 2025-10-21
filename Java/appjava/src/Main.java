d4b62526b4f8a67ab0fe00548b2d316ef351183e
Antonio <antoniomrm21@gmail.com>
2025-10-21 12:09:49 +0200

    aplicación de calculadora, Academia Hola Mundo

--- Java/appjava/src/Main.java
+++ Java/appjava/src/Main.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        /**
+         * Crear una calculadora que reciba
+         * 2 numeros y luego imprima el resultado de la
+         * suma de dos números
+         */
+        System.out.println("Esta es una aplicación de calculadora");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingresa tu edad: ");
-        byte valor = sc.nextByte();
-        System.out.println("Tu edad es " + valor);
+        System.out.println("Ingrese el primer número para la suma: ");
+        int num1 = sc.nextInt();
+        System.out.println("Ingrese el segundo número para la suma: ");
+        int num2 = sc.nextInt();
+        System.out.println("La suma de los dos números es: " + (num1 + num2));
     }
 }
