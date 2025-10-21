8562a937fd82da31ab3e8ea0571b2a6d354946a7
Antonio <antoniomrm21@gmail.com>
2025-10-21 11:59:42 +0200

    Clase Scanner en Java, Academia Hola Mundo

--- Java/appjava/src/Main.java
+++ Java/appjava/src/Main.java
@@ -1,14 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println(Math.abs(-15));
-        System.out.println(Math.ceil(10.01));
-        System.out.println(Math.floor(10.9));
-
-        System.out.println(Math.max(10,21));
-        System.out.println(Math.min(21,50));
-        System.out.println(Math.round(15.16));
-
-        double random = Math.random();
-        System.out.println((int) (random * 100));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu edad: ");
+        byte valor = sc.nextByte();
+        System.out.println("Tu edad es " + valor);
     }
 }
