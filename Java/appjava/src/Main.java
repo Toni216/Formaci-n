00cedee69a1d318ed47584d15a3a056cde1dde78
Antonio <antoniomrm21@gmail.com>
2025-10-22 11:11:09 +0200

    Operadores lógicos, Academia Hola mundo

--- Java/appjava/src/Main.java
+++ Java/appjava/src/Main.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        /**
-         * Crear una calculadora que reciba
-         * 2 numeros y luego imprima el resultado de la
-         * suma de dos números
-         */
-        System.out.println("Esta es una aplicación de calculadora");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el primer número para la suma: ");
-        int num1 = sc.nextInt();
-        System.out.println("Ingrese el segundo número para la suma: ");
-        int num2 = sc.nextInt();
-        System.out.println("La suma de los dos números es: " + (num1 + num2));
+        int a = 15;
+        int b = 10;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a >= b);
+        System.out.println(a < b);
+        System.out.println(a <= b);
     }
 }
