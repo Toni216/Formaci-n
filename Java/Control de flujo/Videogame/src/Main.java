b69e7339d17f028b1093b90215d7ddd0dbdce10e
Antonio <antoniomrm21@gmail.com>
2025-10-27 17:57:03 +0100

    Ejercicio 1, Academia Hola Mundo

--- Java/Control de flujo/Videogame/src/Main.java
+++ Java/Control de flujo/Videogame/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        /**
+        /** EJERCICIO 1
          * Crear la clase VideoGame
          * con las propiedades
          * name string
@@ -9,5 +9,9 @@ public class Main {
          *
          * getters y setters para todas las propiedades
          */
+
+        VideoGame juego1 = new VideoGame();
+        juego1.setName("Minecraft");
+        System.out.println(juego1.name);
     }
 }
--- Java/Control de flujo/Videogame/src/VideoGame.java
+++ Java/Control de flujo/Videogame/src/VideoGame.java
@@ -2,4 +2,28 @@ public class VideoGame {
     String name;
     Double price;
     String category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
