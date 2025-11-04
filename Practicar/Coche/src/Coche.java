994a7c0762b72f11651caaabb95386e925a063d7
Antonio <antoniomrm21@gmail.com>
2025-11-04 12:29:56 +0100

    Clase coche

--- Practicar/Coche/src/Coche.java
+++ Practicar/Coche/src/Coche.java
@@ -1,7 +1,64 @@
 public class Coche {
     String marca;
     String modelo;
+    int año;
     double velocidadActual = 0;
+
+    Coche(String marca, String modelo, int año, double velocidadActual) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.velocidadActual = velocidadActual;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void acelerar(int incremento) {
+        this.velocidadActual = velocidadActual + incremento;
+    }
+
+    public void frenar(int decremento) {
+        if (velocidadActual >= decremento) {
+            this.velocidadActual = velocidadActual - decremento;
+        } else {
+            System.out.println("No se puede frenar tanto.");
+        }
+
+    }
+
+    public double obtenerVelocidad() {
+        return velocidadActual;
+    }
+
+    public void mostrarEstado() {
+        System.out.println("== Estado del vehículo: ==");
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Año: " + año);
+        System.out.println("Velocidad actual: " + obtenerVelocidad());
+    }
 
 
 }
--- Practicar/Coche/src/Main.java
+++ Practicar/Coche/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Crea una clase Coche con:
+        // - Atributos: marca, modelo, año, velocidadActual
+        // - Métodos:
+        //   • acelerar(int incremento)
+        //   • frenar(int decremento) - la velocidad no puede ser negativa
+        //   • obtenerVelocidad()
+        //   • mostrarEstado()
+        Coche coche = new Coche("Toyota", "Camris", 2020, 0);
+        System.out.println(coche.año);
+        coche.acelerar(20);
+        System.out.println(coche.obtenerVelocidad());
+        coche.acelerar(200);
+        System.out.println(coche.obtenerVelocidad());
+        coche.frenar(100);
+        System.out.println(coche.obtenerVelocidad());
+        coche.frenar(125);
+        System.out.println(coche.obtenerVelocidad());
     }
 }
