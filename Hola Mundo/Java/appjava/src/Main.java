32d965bfb9e7855f5c29b8b98edb9c204504f40c
Antonio <antoniomrm21@gmail.com>
2025-11-17 13:17:25 +0100

    Implementando multiples interfaces

--- Hola Mundo/Java/appjava/src/Main.java
+++ Hola Mundo/Java/appjava/src/Main.java
@@ -7,7 +7,11 @@ public class Main {
         Garzon g1 = new Garzon();
         Garzon g2 = new Garzon();
         Garzon g3 = new Garzon();
+        Cocinero c = new Cocinero();
 
-        r.llevarCervezas(new Garzon[]{ g1, g2, g3 });
+        r.llevarCervezas(new Atendible[]{ g1, g2, g3 });
+
+        Cocinable cs = new Cocinero();
+        cs.cocinar();
     }
 }
