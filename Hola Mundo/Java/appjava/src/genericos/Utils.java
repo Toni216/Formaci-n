c64d668e9051a9e7e97ad41df00722545ec8ffd2
Antonio <antoniomrm21@gmail.com>
2025-11-20 12:52:40 +0100

    Wildcards

--- Hola Mundo/Java/appjava/src/genericos/Utils.java
+++ Hola Mundo/Java/appjava/src/genericos/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static <T extends Number> T max(T a, T b) {
         return a.doubleValue() > b.doubleValue() ? a : b;
     }
+
+    public static <T extends Garzon> Lista<T> sorter(Lista<T> lista) {
+        return lista;
+    }
 }
