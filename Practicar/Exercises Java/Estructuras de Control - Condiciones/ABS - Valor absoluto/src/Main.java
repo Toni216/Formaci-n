b28c78c0f0e881f9fce6dad20c5498ee522f6812
Antonio <antoniomrm21@gmail.com>
2025-11-21 12:16:58 +0100

    ABS - Valor absoluto

---
+++ Practicar/Exercises Java/Estructuras de Control - Condiciones/ABS - Valor absoluto/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un número: ");
+        int x = scanner.nextInt();
+
+        System.out.println("El valor absoluto del número introducido es de " + Math.abs(x));
+    }
+}
